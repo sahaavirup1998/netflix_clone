17528d3b8fc09ac7782d946b08fde05e8d1a7ba0
Avirup Saha <sahaavirup1998@gmail.com>
2026-09-05 21:07:29 +0530

    create the backend setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World 2!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
